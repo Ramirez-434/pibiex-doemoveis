94e8a141574c4fec16a6db80fe18fdf819f9eeef
Ramirez-434 <mrbatista274@gmail.com>
2026-06-11 15:35:02 -0300

    fix: import missing ShieldAlert in Navbar

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { Menu, X, User, MessageCircle } from 'lucide-react';
+import { Menu, X, User, MessageCircle, ShieldAlert } from 'lucide-react';
 import NotificationBell from './NotificationBell';
 
 const Navbar = () => {
